619d443eb13d3f2d1d4ef20daae6fa86cb0277a7
yhlee026 <leeyheric2013@gmail.com>
2024-08-16 10:53:09 +0800

    database edited

---
+++ db.js
@@ -0,0 +1,10 @@
+const sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database("./database.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log("Connected to the database.");
+  }
+});
+
+module.exports = db;
