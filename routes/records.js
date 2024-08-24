b0fb69112346f8803ab8b742d0f2689788eda790
orangefosh <frozenlim98@gmail.com>
2024-08-25 05:37:37 +0800

    Updated medical records to reflect records in database

--- routes/records.js
+++ routes/records.js
@@ -2,7 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("records.ejs"); // Render the records
+  const userEmail = req.session.user.email;
+  const db = global.db;
+
+  db.all(
+    "SELECT id, date, type, doctor FROM med_records WHERE user = ? ORDER BY date DESC LIMIT 5;",
+    [userEmail],
+    (err, records) => {
+      if (err) {
+        console.error(err.message);
+        return res
+          .status(500)
+          .send("An error occurred while retrieving records");
+      }
+
+      res.render("records.ejs", { records });
+    }
+  );
 });
 
 module.exports = router;
